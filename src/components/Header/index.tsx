c7ef3e49a109a11c4ec86a96d31c926e6c128e90
fe-jcorreia <fe.jcorreia@gmail.com>
2021-04-29 18:17:39 -0300

    dynamic pages added, starting responsive implementation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,47 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Icon, Image, Link } from "@chakra-ui/react";
+import { useRouter } from "next/dist/client/router";
+import { FiChevronLeft } from "react-icons/fi";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
-    <Flex
-      as="header"
-      w="100%"
-      maxW="full"
-      h="100px"
-      px="6"
-      align="center"
-      justify="center"
-      bg="gray.100"
-      boxShadow="base"
-    >
-      <Image src="/logo.svg" />
-    </Flex>
+    <>
+      {asPath !== "/" ? (
+        <Flex
+          w="100vw"
+          maxW="full"
+          h="100px"
+          px="32"
+          align="center"
+          justify="start"
+          bg="gray.100"
+          boxShadow="base"
+        >
+          <Link href="/">
+            <Icon as={FiChevronLeft} fontSize="2rem" />
+          </Link>
+          <Image
+            src="/logo.svg"
+            position="absolute"
+            left="50%"
+            transform="translateX(-50%)"
+          />
+        </Flex>
+      ) : (
+        <Flex
+          w="100vw"
+          maxW="full"
+          h="100px"
+          px="6"
+          align="center"
+          justify="center"
+          bg="gray.100"
+          boxShadow="base"
+        >
+          <Image src="/logo.svg" />
+        </Flex>
+      )}
+    </>
   );
 }
--- src/pages/[continent].tsx
+++ src/pages/[continent].tsx
@@ -1,4 +1,8 @@
+import { Box, Flex, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import { Header } from "../components/Header";
+import ReactCountryFlag from "react-country-flag";
 
 interface ContinentProps {
   id: number;
@@ -22,8 +26,122 @@ interface ContinentPageProps {
 }
 
 export default function ContinentPage({ continent }: ContinentPageProps) {
-  console.log(continent);
-  return <h1>{continent.name}</h1>;
+  return (
+    <Flex direction="column" mb="12">
+      <Head>
+        <title>page | World Trip</title>
+      </Head>
+      <Header />
+      <Flex h="500px" pos="relative">
+        <Image
+          src={continent?.url_nation_img}
+          alt={`${continent?.name}`}
+          objectFit="cover"
+          filter="brightness(0.5)"
+        />
+        <Text
+          as="header"
+          position="absolute"
+          left="32"
+          bottom="4rem"
+          fontSize="5xl"
+          fontWeight="600"
+          color="gray.50"
+        >
+          {continent?.title}
+        </Text>
+      </Flex>
+      <Flex mx="32" my="4rem" align="center" justify="space-between">
+        <Text fontSize="xl" w="600px" textAlign="justify">
+          {continent?.description}
+        </Text>
+        <Flex align="center" justify="space-between">
+          <Box w="8.5rem" fontWeight="600" fontSize="xl" textAlign="center">
+            <Text color="orange.800" fontSize="5xl">
+              {continent?.num_countries}
+            </Text>
+            países
+          </Box>
+          <Box w="8.5rem" fontWeight="600" fontSize="xl" textAlign="center">
+            <Text color="orange.800" fontSize="5xl">
+              {continent?.num_languages}
+            </Text>
+            línguas
+          </Box>
+          <Box w="8.5rem" fontWeight="600" fontSize="xl" textAlign="center">
+            <Text color="orange.800" fontSize="5xl">
+              {continent?.num_city_most_visited}
+            </Text>
+            cidades +100
+          </Box>
+        </Flex>
+      </Flex>
+      <Box mx="32">
+        <Heading fontWeight="500" fontSize="4xl">
+          Cidades +100
+        </Heading>
+        <SimpleGrid flex="1" gap="20" columns={4} align="flex-start" mt="3rem">
+          {continent?.cities.map((cityElement) => {
+            return (
+              <Box
+                h="280px"
+                w="256px"
+                bg="white"
+                key={cityElement.country_code}
+              >
+                <Image
+                  src={cityElement?.img_url}
+                  alt={cityElement?.country}
+                  w="100%"
+                  h="62%"
+                  borderRadius="5px 5px 0 0"
+                  objectFit="cover"
+                />
+                <Box
+                  borderWidth="0 1px 1px 1px"
+                  borderRadius="0 0 5px 5px"
+                  borderColor="rgba(255, 186, 8, 0.5)"
+                  h="38%"
+                >
+                  <Flex
+                    align="center"
+                    justify="space-between"
+                    mx="1rem"
+                    pt="1.5rem"
+                  >
+                    <Text fontWeight="600" fontSize="lg">
+                      {cityElement?.city}
+                    </Text>
+                    <ReactCountryFlag
+                      countryCode={cityElement?.country_code}
+                      svg
+                      style={{
+                        width: "30px",
+                        height: "30px",
+                        borderRadius: "50%",
+                        border: "1px solid #DADADA",
+                      }}
+                      title={cityElement?.country_code}
+                    />
+                  </Flex>
+
+                  <Text
+                    fontSize="md"
+                    color="gray.400"
+                    mt="0.5rem"
+                    mx="1rem"
+                    pb="1.5rem"
+                  >
+                    {cityElement?.country}
+                  </Text>
+                </Box>
+              </Box>
+            );
+          })}
+        </SimpleGrid>
+      </Box>
+    </Flex>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -34,8 +152,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const paths = req.map((continent: ContinentProps) => ({
     params: { continent: continent.name },
   }));
-
-  console.log(paths);
 
   return {
     paths: paths,
@@ -51,9 +167,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const continent = req.filter((continent: ContinentProps) =>
     continent.name === params?.continent ? continent : ""
   )[0];
-
-  console.log(continent);
-  console.log(params);
 
   return {
     props: { continent },
